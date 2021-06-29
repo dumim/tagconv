7a242107f8338136b37156ba772df7c5babb76e7
Dumindu Madithiyagasthenna <dumindudm@gmail.com>
2021-06-29 14:07:36 +1000

    Todo added

--- map.go
+++ map.go
@@ -26,6 +26,7 @@ func getMapOfAllKeyValues(s interface{}) (*map[string]interface{}, error) {
 	var vars = make(map[string]interface{}) // this will hold the variables as a map (JSON)
 
 	// TODO: catch panics when reflecting unexported fields
+	// TODO: handle arrays
 
 	// get value of object
 	t := reflect.ValueOf(s)
